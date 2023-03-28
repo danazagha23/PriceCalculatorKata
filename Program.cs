e9293568520c65d4e89c3d045de83293d1f96b5a
Dana Zagha <dana.zagha0@gmail.com>
2023-03-28 22:17:51 +0300

    modifications on tax

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using PriceCalculatorSolution;
 
 namespace KataProgram
 {
     public class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            Tax tax = new Tax(20);
-            Product product = new Product("Book", 12345, 20.25M);
-
-            Console.WriteLine(product.DisplayProductPrice());
+            Console.WriteLine("Hello, World!");
         }
     }
 }
