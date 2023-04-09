d2bd4809aa2384685de8250baff166b9dca2c5ff
Dana Zagha <dana.zagha0@gmail.com>
2023-04-10 02:16:10 +0300

    refactoring

---
+++ RepositoryClasses/UPCDiscountRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculatorSolution
+{
+    public class UPCDiscountRepository
+    {
+        public static Dictionary<int, int> GetAll()
+        {
+            return new Dictionary<int, int>
+            {
+                {12345, 7},
+                {789, 7 }
+            };
+        }
+    }
+}
